3988cd2492c296f68f003c2731d38ae16df89d9b
Patrick Cho <patrickjohncho@gmail.com>
2024-11-29 13:28:27 -0500

    Fixed naming for weather type

--- countries_explorer/src/types/Weather.ts
+++ countries_explorer/src/types/Weather.ts
@@ -1,5 +1,5 @@
 //This  file contains the interface definiton for the weather type, ensuring type safety when interacting with this type
-export interface Country {
+export interface weather {
     temperature: number;
     condition: string;
     icon: string;
